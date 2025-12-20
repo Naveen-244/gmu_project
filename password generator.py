42e2ad49435c82eba972727489c0c60476ab2737
U Naveen Kumar <unaveenkumar7210@gmail.com>
2025-12-20 14:37:29 +0530

    commit message

---
+++ password generator.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+print("----- Password Generator -----")
+
+length = int(input("Enter password length: "))
+
+# Characters to choose from
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+print("Generated Password:", password)
